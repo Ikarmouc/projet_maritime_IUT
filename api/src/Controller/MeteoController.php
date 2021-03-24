ed1b69a201eeae1558f523e0c079ec7c9c60af6b
Alexandre Dufau <alexandre.dufau@etudiant.univ-lr.fr>
2021-03-24 13:02:04 +0100

    Adding MeteoController with today's forecast values

--- api/src/Controller/MeteoController.php
+++ api/src/Controller/MeteoController.php
@@ -6,30 +6,50 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\Serializer\Serializer;
 
 
 class MeteoController extends AbstractController
-{
+{/*
     /**
      * @Rest\View()
      * @Rest\Get ("/museeMaritime/meteo", name="meteo")
-     */
+     *//*
     public function index(): Response
     {
         return $this->render('meteo/index.html.twig', [
             'controller_name' => 'MeteoController',
         ]);
-    }
+    }*/
 
     /**
      * @Rest\View()
      * @Rest\Get ("/museeMaritime/meteo", name="meteo")
      */
-    public function getThreeDaysForecast(){
+    public function getTodaysForecast(){
         $url = "http://api.openweathermap.org/data/2.5/weather?lat=46.160329&lon=-1.151139&units=metric&appid=174a946e97d5fe1016efe30dc8f5fe76";
         $raw = file_get_contents($url);
-        $json = json_decode($raw);
-        print($json->weather);
+        $data = json_decode($raw);
+        $forecastIcon = "http://openweathermap.org/img/w/"  . $data->{'weather'}[0]->{'icon'} . ".png";
+        $forecastCurrentTemp = $data->{'main'}->{'temp'}."°C";
+        $forecastDescription = $data->{'weather'}[0]->{'description'};
+        $forecastFeelsLikeTemp = $data->{'main'}->{'feels_like'}."°C";
+        $forecastMaxTemp = $data->{'main'}->{'temp_max'}."°C";
+        $forecastMinTemp = $data->{'main'}->{'temp_min'}."°C";
+        $forecastWindSpeed = $data->{'wind'}->{'speed'}."km/h";
+        $forecastHumidity = $data->{'main'}->{'humidity'}."%";
+
+        /*
+        print($forecastMaxTemp);
+        print($forecastMinTemp);
+        print($forecastWindSpeed);
+        print($forecastHumidity);
+        print($forecastFeelsLikeTemp);
+        print($forecastIcon);
+        print($forecastCurrentTemp);
+        print($forecastDescription);
+        */
+
         return $this->render('meteo/index.html.twig', [
             'controller_name' => 'MeteoController',
         ]);
