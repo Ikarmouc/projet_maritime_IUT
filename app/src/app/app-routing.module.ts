3f3a60bac66efd78c6fc0a2565383085a0675ba2
Ismail Karmouche <ismail.karmouche@etudiant.univ-lr.fr>
2021-03-26 11:40:17 +0100

    Config Angular routes + footer app

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component'
+import {MenuBasComponent} from "./menu-bas/menu-bas.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/Menu-Bas',
+    component: MenuBasComponent,
+  },
+  {
+    path: '',
+    component: DashboardComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
 })
 export class AppRoutingModule { }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBasComponent } from './menu-bas/menu-bas.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBasComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
