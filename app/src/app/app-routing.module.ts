34b2418f58962232b943c6c678ce6e875c5484cc
Ismail Karmouche <ismail.karmouche@etudiant.univ-lr.fr>
2021-03-26 12:10:45 +0100

    Mise a jour routes et contenu menu bas

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component'
-import {MenuBasComponent} from "./menu-bas/menu-bas.component";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {MenuBasComponent} from './menu-bas/menu-bas.component';
 
 const routes: Routes = [
   {
-    path: '/Menu-Bas',
+    path: '/app/musee/accueil',
     component: MenuBasComponent,
   },
   {
-    path: '',
+    path: 'dashboard',
     component: DashboardComponent,
   },
 ];
