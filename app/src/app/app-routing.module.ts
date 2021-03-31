58db59c07df1dca02ed03aecd3ea6fa6bc18c2eb
Ismail Karmouche <ismail.karmouche@etudiant.univ-lr.fr>
2021-03-31 15:00:54 +0200

    US5 done

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import {MenuBasComponent} from './menu-bas/menu-bas.component';
 
 const routes: Routes = [
-  {
-    path: '/app/musee/accueil',
-    component: MenuBasComponent,
-  },
+
 ];
 
 @NgModule({
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBasComponent } from './menu-bas/menu-bas.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBasComponent,
-    DashboardComponent
+    MenuBasComponent
   ],
   imports: [
     BrowserModule,
