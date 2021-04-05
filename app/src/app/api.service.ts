a10853f4c7bdfc45b5e70a3ca12e6deff186e6f5
Enzo Naullet <enzo.naullet@etudiant.univ-lr.fr>
2021-04-05 19:29:25 +0200

    Add markers

--- app/src/app/api.service.ts
+++ app/src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 // import { Bateau } from '../../../model/bateau.model';
-import { localisationBateau } from '../../../model/localisationBateau.model';
+// import { localisationBateau } from '../../../model/localisationBateau.model';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
 
@@ -10,7 +10,7 @@ import {Observable} from 'rxjs';
 export class ApiService {
   constructor(private httpClient: HttpClient) {
   }
-  getBateaux(): Observable<any> {
+  /*getBateaux(): Observable<any> {
     return this.httpClient.get<localisationBateau>('http://localhost:9999/api/musee/bateau');
-  }
+  }*/
 }
